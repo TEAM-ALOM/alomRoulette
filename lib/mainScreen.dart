e0fe6b2438cdcd1472cad30e1f8dd5a85f57670b
Daniel Kim <dwkim0101@naver.com>
2024-02-07 13:30:28 +0900

    bottom bar update

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,44 +1,132 @@
+import 'package:animated_notch_bottom_bar/animated_notch_bottom_bar/animated_notch_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:sedong_mbti/const/sizes.dart';
 
-class mainScreen extends StatelessWidget {
+class mainScreen extends StatefulWidget {
   const mainScreen({super.key});
+
+  @override
+  State<mainScreen> createState() => _mainScreenState();
+}
+
+class _mainScreenState extends State<mainScreen> {
+  /// Controller to handle PageView and also handles initial page
+  final _pageController = PageController(initialPage: 0);
+
+  /// Controller to handle bottom nav bar and also handles initial page
+  final _controller = NotchBottomBarController(index: 0);
+
+  int maxCount = 5;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  /// widget list
+  final List<Widget> bottomBarPages = [
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+    const Placeholder(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: (bottomBarPages.length <= maxCount)
+          ? AnimatedNotchBottomBar(
+              /// Provide NotchBottomBarController
+              notchBottomBarController: _controller,
+              color: Colors.white,
+              showLabel: false,
+              shadowElevation: 5,
+              kBottomRadius: 28.0,
+              // notchShader: const SweepGradient(
+              //   startAngle: 0,
+              //   endAngle: pi / 2,
+              //   colors: [Colors.red, Colors.green, Colors.orange],
+              //   tileMode: TileMode.mirror,
+              // ).createShader(Rect.fromCircle(center: Offset.zero, radius: 8.0)),
+              notchColor: Colors.black87,
+
+              /// restart app if you change removeMargins
+              removeMargins: false,
+              bottomBarWidth: 500,
+              durationInMilliSeconds: 300,
+              bottomBarItems: const [
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.home_filled,
+                    color: Colors.blueAccent,
+                  ),
+                  itemLabel: 'Page 1',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.star,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.star,
+                    color: Colors.blueAccent,
+                  ),
+                  itemLabel: 'Page 2',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.settings,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.settings,
+                    color: Colors.pink,
+                  ),
+                  itemLabel: 'Page 4',
+                ),
+                BottomBarItem(
+                  inActiveItem: Icon(
+                    Icons.person,
+                    color: Colors.blueGrey,
+                  ),
+                  activeItem: Icon(
+                    Icons.person,
+                    color: Colors.yellow,
+                  ),
+                  itemLabel: 'Page 5',
+                ),
+              ],
+              onTap: (index) {
+                /// perform action on tab change and to update pages you can update pages without pages
+                // log('current selected index $index');
+                _pageController.jumpToPage(index);
+              },
+              kIconSize: 24.0,
+            )
+          : null,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         toolbarHeight: 0,
         // backgroundColor: Colors.black,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size20,
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: Sizes.size14 + Sizes.size16,
-            ),
-            Container(
-              child: Text(
-                '잠깐! 인스타 팔로우 하셨나요?',
-                style: Text_style(context),
-              ),
-            ),
-            SizedBox(
-              height: Sizes.size14 + Sizes.size16,
-            ),
-          ],
-        ),
+      body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: List.generate(
+            bottomBarPages.length, (index) => bottomBarPages[index]),
       ),
+      extendBody: true,
     );
   }
 
   TextStyle Text_style(BuildContext context) {
     return TextStyle(
-      shadows: [
+      shadows: const [
         Shadow(
           color: Colors.grey,
           offset: Offset(3.0, 3.0),

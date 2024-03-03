7dc46ed6377fff1b610bff406fc8cc8110fbb647
Daniel Kim <dwkim0101@naver.com>
2024-03-04 03:35:28 +0900

    *ERROR FIXED* Timer crashes on dispose

--- lib/view/money_panel.dart
+++ lib/view/money_panel.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:animated_flip_counter/animated_flip_counter.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 class MoneyPanelWidget extends StatefulWidget {
@@ -12,18 +13,33 @@ class MoneyPanelWidget extends StatefulWidget {
 }
 
 class _MoneyPanelWidgetState extends State<MoneyPanelWidget> {
+  late ConfettiController _controllerCenter;
   int moneyState = 0;
   int _seconds = 0;
   bool _isRunning = false;
   late Timer _timer;
+  @override
+  void initState() {
+    super.initState();
+    _controllerCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // _timer.cancel();
+    _controllerCenter.dispose();
+  }
 
   void _startTimer() {
     _isRunning = true;
-    _timer = Timer.periodic(const Duration(microseconds: 100), (timer) {
+    _seconds = 0;
+    _timer = Timer.periodic(const Duration(microseconds: 3000), (timer) {
       setState(() {
-        _seconds = Random().nextInt(1000);
-        // _seconds++;
-        if (_seconds == 1000) {
+        // _seconds = Random().nextInt(1000);
+        _seconds += 111;
+        if (_seconds >= 1000) {
           _seconds = 0;
         }
       });
@@ -32,7 +48,37 @@ class _MoneyPanelWidgetState extends State<MoneyPanelWidget> {
 
   void _stopTimer() {
     _isRunning = false;
+    _controllerCenter.play();
+
+    setState(() {
+      // _seconds = Random().nextInt(1000);
+    });
+
     _timer.cancel();
+  }
+
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
   }
 
   @override
@@ -42,15 +88,52 @@ class _MoneyPanelWidgetState extends State<MoneyPanelWidget> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedFlipCounter(
-              duration: const Duration(milliseconds: 1500),
-              value: _seconds, // pass in a value like 2014
-              textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 180,
-              ),
+            Row(
+              children: [
+                AnimatedFlipCounter(
+                  curve: Curves.ease,
+                  duration: const Duration(milliseconds: 400),
+                  value: _seconds ~/ 100, // pass in a value like 2014
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 180,
+                  ),
+                ),
+                AnimatedFlipCounter(
+                  duration: const Duration(milliseconds: 600),
+                  value: (_seconds ~/ 10) % 10, // pass in a value like 2014
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 180,
+                  ),
+                ),
+                AnimatedFlipCounter(
+                  duration: const Duration(milliseconds: 1000),
+                  value: _seconds % 10, // pass in a value like 2014
+                  textStyle: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 180,
+                  ),
+                ),
+              ],
             ),
             const SizedBox(height: 16),
+            ConfettiWidget(
+              emissionFrequency: 0, // how often it should emit
+              numberOfParticles: 200, // number of particles to emit
+              gravity: 0.05, // grav
+              confettiController: _controllerCenter,
+              blastDirectionality: BlastDirectionality.explosive,
+
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink,
+                Colors.orange,
+                Colors.purple
+              ],
+              createParticlePath: drawStar,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
